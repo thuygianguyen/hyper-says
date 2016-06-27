fcb0debc81c09399c52b3f81d0b475cfe9306f64
Johannes Gorset <jgorset@gmail.com>
2016-06-27 15:12:42 +0200

    Initial commit

---
+++ quotes.js
@@ -0,0 +1,110 @@
+var quotes = [
+  {
+    quote: "You have to understand what you are doing!",
+    author: "Felipe"
+  },
+  {
+    quote: "Abstract yourself!",
+    author: "Felipe"
+  },
+  {
+    quote: "Sometimes, I do create bugs!",
+    author: "Felipe"
+  },
+  {
+    quote: "I think no one knows that it doesn't work, but I can't sleep",
+    author: "Vadym"
+  },
+  {
+    quote: "The toilet is on fire!",
+    author: "Knut"
+  },
+  {
+    quote: "This is why I @hyperoslo",
+    author: "Johannes"
+  },
+  {
+    quote: "The devil is in the details",
+    author: "Marte"
+  },
+  {
+    quote: "Dude!",
+    author: "Rrramon"
+  },
+  {
+    quote: "Keep calm and carry on",
+    author: "Rolf"
+  },
+  {
+    quote: "I have a meeting",
+    author: "Rolf"
+  },
+  {
+    quote: "Børk! Børk! Børk!",
+    author: "Chris"
+  },
+  {
+    quote: "That's the thing!",
+    author: "Rrramon"
+  },
+  {
+    quote: "You'll get used to it!",
+    author: "Mathias"
+  },
+  {
+    quote: "Where should we place Russia?",
+    author: "Mads"
+  },
+  {
+    quote: "Johannes did a fantastic",
+    author: "Einar"
+  },
+  {
+    quote: "The only constant is change",
+    author: "Øistein"
+  },
+  {
+    quote: "Only alcohol is not enough.",
+    author: "Ragnhild"
+  },
+  {
+    quote: "We don't tweak pixels anymore, we twerk them",
+    author: "Thuy"
+  },
+  {
+    quote: "I just want to be in the list of contributors",
+    author: "Vadym"
+  },
+  {
+    quote: "There is one weird thing, I didn’t want to tell you.",
+    author: "Vadym"
+  },
+  {
+    quote: "It gives me great pleasure and enthusiasm to apply for a suitable post in your esteemed organization.",
+    author: "Every job applicant for the tech team ever"
+  },
+  {
+    quote: "Stop quoting me for everything",
+    author: "Felipe"
+  },
+  {
+    quote: "Please update your profile picture or face",
+    author: "Mathias"
+  },
+  {
+    quote: "I owe you a teabag, Tim.",
+    author: "Johannes"
+  },
+  {
+    quote: "I'm not Vadym, I'm Ramon",
+    author: "Vadym"
+  },
+  {
+    quote: "I don’t remember exactly every line of code and logic in the whole project",
+    author: "Felipe"
+  },
+  {
+    quote: "It's like having the power of the sun in your pants",
+    author: "Jonas"
+  },
+]
---
+++ scripts.js
@@ -0,0 +1,88 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var querySelector = document.querySelector.bind(document);
+
+  function random(min, max) {
+    return min + Math.floor(Math.random() * (max - min + 1));
+  }
+
+  function shuffle(array) {
+    var index = -1;
+    var length = array.length;
+    var result = array.slice(0);
+
+    while (++index < length) {
+      var rand = random(index, length - 1);
+      var value = result[rand];
+
+      result[rand] = result[index];
+      result[index] = value;
+    }
+
+    result.length = length;
+
+    return result;
+  }
+
+  function rotate(array, fn) {
+    var value = array.pop();
+
+    fn(value);
+
+    array.unshift(value);
+  }
+
+  function setBackground(color) {
+    document.body.style.background = 'hsl(' + color + ', 50%, 55%)';
+  }
+
+  function setQuote(quote) {
+    var author = querySelector('.author');
+    var blockquote = querySelector('blockquote');
+    var content = querySelector('.quote');
+
+    blockquote.classList.add('flipOutX');
+
+    setTimeout(function() {
+      author.textContent = '— ' + quote.author;
+
+      blockquote.classList.remove('flipOutX');
+      blockquote.classList.add('flipInX');
+
+      content.textContent = quote.quote;
+      content.classList.add('notation');
+    }, 1000);
+  }
+
+  var quotes = shuffle(window.quotes);
+  var colors = shuffle([180, 163, 205, 200, 168, 357, 297, 187, 319]);
+
+  function changeQuote() {
+    // Prevent switching quote and color if event is triggered by text selection
+    if (getSelection().toString()) return;
+
+    rotate(colors, setBackground);
+    rotate(quotes, setQuote);
+  }
+
+  var interval = null;
+
+  var startInterval = function() {
+    clearInterval(interval);
+
+    changeQuote();
+
+    interval = setInterval(changeQuote, 10000);
+  };
+
+  rotate(colors, setBackground);
+
+  setTimeout(function() {
+    startInterval();
+
+    document.body.addEventListener('click', startInterval);
+  }, 3000);
+
+  if (window.location.hash.toLowerCase() === '#tv') {
+    querySelector('.links').style.display = 'none';
+  }
+});
